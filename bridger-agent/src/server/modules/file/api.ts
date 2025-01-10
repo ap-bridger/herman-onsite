0283dcbf527df0f88e0784d89b0592546258d030
Herman Ho <hho@attentivemobile.com>
2025-01-10 15:22:55 -0500

    uploading to table

--- bridger-agent/src/server/modules/file/api.ts
+++ bridger-agent/src/server/modules/file/api.ts
@@ -1,15 +1,26 @@
 import { prisma } from "@/lib/db"
+import { parse } from "fast-csv";
+import moment from "moment";
 
 export const file = async (_: any, args: {contents: string}) => {
 
   const decodedContents = Buffer.from( args.contents, "base64").toString("utf-8")
-  console.log("contents: " + decodedContents)
+  // console.log("contents: " + decodedContents)
 
-  // await prisma.file.create({
-  //   data: {
-  //     type: "type", name: "name"
-  //   }
-  // });
+  const stream = parse({ headers: ['date', 'description', 'payee', 'category', 'spent', 'received'], renameHeaders: true }).on('data',  row => {
+    prisma.pendingTransactions.create({
+      data: {
+        date: moment(row.date, "mm/dd/yyyy").toISOString(),
+        description: row.description,
+        payee: row.payee,
+        accountCategory: row.category,
+        spent: parseInt(row.spent?.replace("$", "").replace(",", "").replace(".", "")),
+        received: parseInt(row.received?.replace("$", "").replace(",", "").replace(".", "")),
+      }
+    }).catch(reason => console.log("Failed to save: " + reason));
+  });
+  stream.write(decodedContents);
+  stream.end();
 
   return "";
 };
